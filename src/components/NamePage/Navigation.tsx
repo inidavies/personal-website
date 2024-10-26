8572c1488a64e4780416ff3c4658df8c006d3a62
Inioluwa Davies <inidavies24@gmail.com>
2024-10-26 18:26:41 -0500

    some style changes

--- src/components/NamePage/Navigation.tsx
+++ src/components/NamePage/Navigation.tsx
@@ -6,14 +6,17 @@ import ProfilePic from "./ProfilePic";
 
 export default function Navigation() {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
+    <Navbar fixed="top" expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">
+        <Navbar.Brand href="/">
           <ProfilePic />
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse
+          id="responsive-navbar-nav"
+          style={{ justifyContent: "end" }}
+        >
+          <Nav>
             <Nav.Link href="#about">about</Nav.Link>
             <Nav.Link href="#skills">skills</Nav.Link>
             <Nav.Link href="#projects">projects</Nav.Link>
