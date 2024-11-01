77e03fc6a39a3fc76d074fdd371d5243dd93f610
Inioluwa Davies <inidavies24@gmail.com>
2024-11-01 16:12:00 -0500

    added some real and dummy content

--- src/components/AboutPage/index.tsx
+++ src/components/AboutPage/index.tsx
@@ -1,7 +1,28 @@
+import Image from "next/image";
+import React from "react";
+
 function AboutPage() {
   return (
-    <div id="about" className="mypage alternate-page">
-      <h1>About</h1>
+    <div id="about" className="mypage alternate-page aboutAndSkills">
+      <Image
+        src="/images/profile.jpg"
+        height={600}
+        width={500}
+        id="aboutPic"
+        alt="a picture of me"
+      />
+      <div id="aboutText">
+        <h2>a little about me</h2>
+        <p>
+          I am a BSc in Computer Science graduate from University of North
+          Texas. I am a BSc in Computer Science graduate from University of
+          North Texas. I am a BSc in Computer Science graduate from University
+          of North Texas. I am a BSc in Computer Science graduate from
+          University of North Texas. I am a BSc in Computer Science graduate
+          from University of North Texas. I am a BSc in Computer Science
+          graduate from University of North Texas
+        </p>
+      </div>
     </div>
   );
 }
--- src/components/SkillsPage/index.tsx
+++ src/components/SkillsPage/index.tsx
@@ -1,7 +1,47 @@
 function SkillsPage() {
   return (
     <div id="skills" className="mypage alternate-page">
-      <h1>Skills</h1>
+      <div>
+        <h2>my tech stack</h2>
+        <p>
+          I am a BSc in Computer Science graduate from University of North
+          Texas. I am a BSc in Computer Science graduate from University of
+          North Texas. I am a BSc in Computer Science graduate from University
+          of North Texas.
+        </p>
+      </div>
+      <div>
+        {/*make this a iteration on a list */}
+        <ul>
+          <li>
+            <h2>Python</h2>
+          </li>
+          <li>
+            <h2>HTML</h2>
+          </li>
+          <li>
+            <h2>TailwindCSS</h2>
+          </li>
+          <li>
+            <h2>JavaScript</h2>
+          </li>
+          <li>
+            <h2>React JS</h2>
+          </li>
+          <li>
+            <h2>React Native</h2>
+          </li>
+          <li>
+            <h2>Flask</h2>
+          </li>
+          <li>
+            <h2>Jinja</h2>
+          </li>
+          <li>
+            <h2>Figma</h2>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
