66952e9c295568152c229517c9b73124e99e7654
Inioluwa Davies <inidavies24@gmail.com>
2024-11-03 17:50:07 -0600

    make about responsive

--- src/components/AboutPage/index.tsx
+++ src/components/AboutPage/index.tsx
@@ -3,7 +3,9 @@ import React from "react";
 function AboutPage() {
   return (
     <div id="about" className="mypage alternate-page aboutAndSkills">
-      <img src="/images/aboutPic.jpg" id="aboutPic" alt="a picture of me" />
+      <div id="aboutPicContainer">
+        <img src="/images/aboutPic.jpg" id="aboutPic" alt="a picture of me" />
+      </div>
       <div id="aboutText">
         <h2>a little about me</h2>
         <p>
