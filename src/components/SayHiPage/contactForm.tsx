369c1c316ecbd1df09d37cc5b07e749afe2de75a
Inioluwa Davies <inidavies24@gmail.com>
2025-01-17 16:56:35 -0600

    make expandable divs for projects

--- src/components/SayHiPage/contactForm.tsx
+++ src/components/SayHiPage/contactForm.tsx
@@ -1,5 +1,5 @@
 import emailjs from "@emailjs/browser";
-import { use, useState } from "react";
+import { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 
