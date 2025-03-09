eb57d69db8618b99bb7e53096862503993995771
Inioluwa Davies <inidavies24@gmail.com>
2025-03-08 21:31:34 -0600

    add footer

--- src/components/SayHiPage/index.tsx
+++ src/components/SayHiPage/index.tsx
@@ -1,11 +1,15 @@
 import ContactForm from "./contactForm";
 import ContactLinks from "./contactLinks";
+import Footer from "./pageFooter";
 
 function SayHiPage() {
   return (
-    <div id="sayHi" className="mypage alternate-page">
-      <ContactForm />
-      <ContactLinks />
+    <div id="sayHiContainer">
+      <div id="sayHi" className="mypage alternate-page">
+        <ContactForm />
+        <ContactLinks />
+      </div>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/SayHiPage/pageFooter.tsx
@@ -0,0 +1,12 @@
+export default function Footer() {
+  return (
+    <footer id="footer">
+      <p id="copyrightText">
+        © 2025 <span className="fw-bold">Ini Davies</span>
+      </p>
+      <a href="#" id="backToTop">
+        BACK TO TOP
+      </a>
+    </footer>
+  );
+}
