bea08bb9bd86891d49ed68ad3aaed66b3bfa3563
Inioluwa Davies <inidavies24@gmail.com>
2025-03-08 19:59:07 -0600

    add style changes

--- src/components/ProjectsPage/index.tsx
+++ src/components/ProjectsPage/index.tsx
@@ -3,7 +3,7 @@ import { Project } from "./Project";
 
 function ProjectsPage() {
   return (
-    <div id="projectsPage" className="mypage">
+    <div id="projects" className="mypage">
       <Project
         projectIndex={1}
         projectName={"PERSONAL WEBSITE"}
@@ -12,21 +12,44 @@ function ProjectsPage() {
         projectTechStack={["HTML", "CSS", "REACT JS", "TYPESCRIPT", "FIGMA"]}
         projectLink="https://github.com/inidavies/personal-website"
         projectContent={
-                <>
-                <h5><strong>Problem Statement</strong></h5>
-                  <div id="pwProblemStatement">
-                    <p className="pwPSContent">This project was created to showcase my skills in front-end development and UI/UX design, and list my projects.
-                    On Figma, I determined the assets, color-scheme, and logo then created low and high-fidelity wireframes and mockups 
-                    to visualize the design and user experience on various screen sizes. I then decided on my tech stack and began development.</p>
+          <>
+            <h5>
+              <strong>Problem Statement</strong>
+            </h5>
+            <div id="pwProblemStatement">
+              <p className="pwPSContent">
+                This project was created to showcase my skills in front-end
+                development and UI/UX design, and list my projects. On Figma, I
+                determined the assets, color-scheme, and logo then created low
+                and high-fidelity wireframes and mockups to visualize the design
+                and user experience on various screen sizes. I then decided on
+                my tech stack and began development.
+              </p>
 
-                    <p className="pwPSContent">My goal was to create a responsive, clean, intuitive, and easily navigable website, that clearly showcases my projects and skills.
-                    This project is not static, I will continuously iterate on it to improve the design as my skills develop.</p>
-                  </div>
-                  <div style={{display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-evenly'}} >
-                    <iframe style={{border: '1px solid rgba(0, 0, 0, 0.1)'}} width="800" height="600" src="https://embed.figma.com/design/mEeVQ6cfpGXO0e57ezC59z/Portfolio-Website?node-id=10-9&embed-host=share" ></iframe>
-                  </div>
-                </>
-          }
+              <p className="pwPSContent">
+                My goal was to create a responsive, clean, intuitive, and easily
+                navigable website, that clearly showcases my projects and
+                skills. This project is not static, I will continuously iterate
+                on it to improve the design as my skills develop.
+              </p>
+            </div>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                alignItems: "center",
+                justifyContent: "space-evenly",
+              }}
+            >
+              <iframe
+                style={{ border: "1px solid rgba(0, 0, 0, 0.1)" }}
+                width="800"
+                height="600"
+                src="https://embed.figma.com/design/mEeVQ6cfpGXO0e57ezC59z/Portfolio-Website?node-id=10-9&embed-host=share"
+              ></iframe>
+            </div>
+          </>
+        }
       />
 
       <Project
@@ -41,39 +64,66 @@ function ProjectsPage() {
         projectTechStack={["REACT NATIVE", "CSS", "TYPESCRIPT"]}
         projectLink="https://scoutmasterapp.com/"
         projectContent={
-                <>
-                <h5><strong>Problem Statement</strong></h5>
-                  <p> 
-                      ScoutMaster was my senior year Computer Science Capstone project, created to be a free GPS mobile application 
-                      for lovers of media! Ever wanted to visit where your favorite film or series was filmed?
-                      ScoutMaster&apos;s intuitive design makes this possible with its easy access to the web&apos;s most comprehensive database 
-                      of media filming locations + its ability to connect & communicate with like-minded pop-culture enthusiasts 
-                      around the world! Users are able to &quot;scout&quot; the locations they have visited and it will appear on their profile.
-                      Our capstone sponsor and founder of ScoutMaster, Matthew Muhl, maintains an ever growing database 
-                      of 1000+ filming locations for movies, shows, and music videos.
-                  </p>
-                  <p>
-                      The motivation for the project was to collaborate 
-                      on the re-launch of ScoutMaster using existing files & assets. 
-                      My team consisted of 8 software engineers, with the aim to enhance the user experience with cleaner user interface 
-                      and smoother performance, while maintaining the essential features that made the app so unique when it originally launched in 2020. 
-                  </p>
-                  <p>
-                      Note: ScoutMaster has been non-operational since 2022 and we developed a new version of the mobile app from scratch. 
-                      The website linked displays the app&apos;s older UI but The new version is not live and 
-                      I&apos;m unable share the assets here for copyright reasons, but I&apos;m happy to discuss technical details.
-                  </p>
-                  <h5><strong>My Role</strong></h5>
-                  <ul>
-                      <li>Designed and developed the front-end of the mobile app using <strong>React Native</strong>, <strong>Typescript</strong>, and <strong>CSS</strong>.</li>
-                      <li>Developed a mobile application compatible with both Android and IOS devices, with design and performance optimized for both operating systems</li>
-                      <li>Collaborated with the back-end team to integrate the front-end with the back-end using <strong>RESTful APIs</strong>.</li>
-                      <li>Conducted user testing and gathered feedback to improve the user experience.</li>
-                  </ul>
-                </>
+          <>
+            <h5>
+              <strong>Problem Statement</strong>
+            </h5>
+            <p>
+              ScoutMaster was my senior year Computer Science Capstone project,
+              created to be a free GPS mobile application for lovers of media!
+              Ever wanted to visit where your favorite film or series was
+              filmed? ScoutMaster&apos;s intuitive design makes this possible
+              with its easy access to the web&apos;s most comprehensive database
+              of media filming locations + its ability to connect & communicate
+              with like-minded pop-culture enthusiasts around the world! Users
+              are able to &quot;scout&quot; the locations they have visited and
+              it will appear on their profile. Our capstone sponsor and founder
+              of ScoutMaster, Matthew Muhl, maintains an ever growing database
+              of 1000+ filming locations for movies, shows, and music videos.
+            </p>
+            <p>
+              The motivation for the project was to collaborate on the re-launch
+              of ScoutMaster using existing files & assets. My team consisted of
+              8 software engineers, with the aim to enhance the user experience
+              with cleaner user interface and smoother performance, while
+              maintaining the essential features that made the app so unique
+              when it originally launched in 2020.
+            </p>
+            <p>
+              Note: ScoutMaster has been non-operational since 2022 and we
+              developed a new version of the mobile app from scratch. The
+              website linked displays the app&apos;s older UI but The new
+              version is not live and I&apos;m unable share the assets here for
+              copyright reasons, but I&apos;m happy to discuss technical
+              details.
+            </p>
+            <h5>
+              <strong>My Role</strong>
+            </h5>
+            <ul>
+              <li>
+                Designed and developed the front-end of the mobile app using{" "}
+                <strong>React Native</strong>, <strong>Typescript</strong>, and{" "}
+                <strong>CSS</strong>.
+              </li>
+              <li>
+                Developed a mobile application compatible with both Android and
+                IOS devices, with design and performance optimized for both
+                operating systems
+              </li>
+              <li>
+                Collaborated with the back-end team to integrate the front-end
+                with the back-end using <strong>RESTful APIs</strong>.
+              </li>
+              <li>
+                Conducted user testing and gathered feedback to improve the user
+                experience.
+              </li>
+            </ul>
+          </>
         }
       />
-      
+
       <Project
         projectIndex={3}
         projectName={"GROCERY TRACKER"}
@@ -83,22 +133,39 @@ function ProjectsPage() {
         projectLink="https://github.com/EthanWillinger/GroceryTracker"
         projectContent={
           <>
-          <h5><strong>Problem Statement</strong></h5>
+            <h5>
+              <strong>Problem Statement</strong>
+            </h5>
             <p>
-              The goal of this project is to reduce food waste and help the user save money. The idea is to implement a web application that: 
+              The goal of this project is to reduce food waste and help the user
+              save money. The idea is to implement a web application that:
             </p>
             <ul>
-              <li>Reads the barcode of grocery items and provides information on the products.</li>
+              <li>
+                Reads the barcode of grocery items and provides information on
+                the products.
+              </li>
               <li>Keeps an inventory of all the entered grocery items.</li>
               <li>Estimates the expiration date of each item in inventory.</li>
-              <li>Notifies the user via email before their groceries expire.</li>
-              
+              <li>
+                Notifies the user via email before their groceries expire.
+              </li>
             </ul>
-            <div style={{display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-evenly'}}
-                >
-                  <iframe style={{border: '1px solid rgba(0, 0, 0, 0.1)'}} width="800" height="600" src="https://embed.figma.com/design/zLNz6ScSGknxwwKBtfDlTL/Grocery-Tracker?embed-host=share"></iframe>
-                  
-                  </div>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                alignItems: "center",
+                justifyContent: "space-evenly",
+              }}
+            >
+              <iframe
+                style={{ border: "1px solid rgba(0, 0, 0, 0.1)" }}
+                width="800"
+                height="600"
+                src="https://embed.figma.com/design/zLNz6ScSGknxwwKBtfDlTL/Grocery-Tracker?embed-host=share"
+              ></iframe>
+            </div>
           </>
         }
       />
